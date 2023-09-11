a12508dbf22bbd74a7371e5c1515f942815e3987
EdPaul1 <saidarajab101@gmail.com>
2023-09-11 16:13:22 +0300

    Error handling for project colors

--- src/helpers/ProjectColors.js
+++ src/helpers/ProjectColors.js
@@ -1,5 +1,12 @@
 import { palette } from '../theme/palette'
 
 export const ProjectColors = (project) => {
-    return palette.find((c) => c.mainColor === project.color)
+    const matchingColor = palette.find((c) => c.mainColor === project.color);
+    if (matchingColor) {
+        return matchingColor;
+    } else {
+        // Handle the case when no matching color is found
+        // You can return a default color or throw an error
+        return palette[0]; // Return the first color as a default
+    }
 }
